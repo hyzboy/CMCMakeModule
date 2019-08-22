f88928d976216b21f3318fb9b1a3256ca4365b41
hyzboy <hyzboy@gmail.com>
2019-08-22 11:36:16 +0800

    改用独立的 math.cmake

--- use_cm_module.cmake
+++ use_cm_module.cmake
@@ -8,12 +8,3 @@ macro(use_cm_module module)
     include(CM${module}/path_config.cmake)
     include_directories(CM${module}/inc)
 endmacro()
-
-macro(use_mgl mgl_path)
-    add_definitions(-DMATH_USE_OPENGL)
-    add_definitions(-DMATH_RIGHTHANDED_CAMERA)
-    add_definitions(-DMATH_AVX)
-    
-    include_directories(${mgl_path}/src)
-    add_subdirectory(${mgl_path})
-endmacro()
