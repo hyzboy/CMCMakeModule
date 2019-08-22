125cccb9c7ea6deaac6b3c7cf6dd0dd5c778eda2
hyzboy <hyzboy@gmail.com>
2019-08-22 11:26:59 +0800

    改进使用方式，增加use_cm_module.cmake

--- compiler.cmake
+++ compiler.cmake
@@ -1,47 +1,43 @@
-﻿macro(set_compiler_param)
+﻿IF(WIN32)
 
-    IF(WIN32)
+    SET(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} /MDd")
+    SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MDd")
 
-        SET(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} /MDd")
-        SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MDd")
+    SET(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} /MD")
+    SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MD")
 
-        SET(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} /MD")
-        SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MD")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++14")
 
-        SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++14")
+    add_compile_options(/arch:AVX2)
+    add_definitions(-D_CRT_SECURE_NO_WARNINGS)
 
-        add_compile_options(/arch:AVX2)
-        add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-
-    ELSE()
-        IF(NOT ANDROID)
-            IF(APPLE)
-                SET(USE_CLANG       ON)
-            ELSE()
-                OPTION(USE_CLANG    OFF)
-            ENDIF()
-
-            if(USE_CLANG)
-                SET(CMAKE_C_COMPILER clang)
-                SET(CMAKE_CXX_COMPILER clang++)
-            endif()
+ELSE()
+    IF(NOT ANDROID)
+        IF(APPLE)
+            SET(USE_CLANG       ON)
+        ELSE()
+            OPTION(USE_CLANG    OFF)
         ENDIF()
 
-        add_compile_options(-mavx)
-
-        SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11")
-
-        SET(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -ggdb3")
-        SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -ggdb3")
-
-        SET(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -Ofast")
-        SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Ofast")
+        if(USE_CLANG)
+            SET(CMAKE_C_COMPILER clang)
+            SET(CMAKE_CXX_COMPILER clang++)
+        endif()
     ENDIF()
 
-    MESSAGE("C Compiler: " ${CMAKE_C_COMPILER})
-    MESSAGE("C++ Compiler: " ${CMAKE_CXX_COMPILER})
-    MESSAGE("C Flag: " ${CMAKE_C_FLAGS})
-    MESSAGE("C++ Flag: " ${CMAKE_CXX_FLAGS})
+    add_compile_options(-mavx)
 
-endmacro()
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11")
+
+    SET(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -ggdb3")
+    SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -ggdb3")
+
+    SET(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -Ofast")
+    SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Ofast")
+ENDIF()
+
+MESSAGE("C Compiler: " ${CMAKE_C_COMPILER})
+MESSAGE("C++ Compiler: " ${CMAKE_CXX_COMPILER})
+MESSAGE("C Flag: " ${CMAKE_C_FLAGS})
+MESSAGE("C++ Flag: " ${CMAKE_CXX_FLAGS})
--- use_cm_module.cmake
+++ use_cm_module.cmake
@@ -1,5 +1,19 @@
+include(compiler)
+include(system_bit)
+include(version)
+include(output_path)
+
 macro(use_cm_module module)
     add_subdirectory(CM${module})
     include(CM${module}/path_config.cmake)
     include_directories(CM${module}/inc)
+endmacro()
+
+macro(use_mgl mgl_path)
+    add_definitions(-DMATH_USE_OPENGL)
+    add_definitions(-DMATH_RIGHTHANDED_CAMERA)
+    add_definitions(-DMATH_AVX)
+    
+    include_directories(${mgl_path}/src)
+    add_subdirectory(${mgl_path})
 endmacro()
