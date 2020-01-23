d4087fe96c0acd43d376ec660bd5f1f351755448
hyzboy <hyzboy@gmail.com>
2020-01-23 20:08:23 +0800

    fix don't use AVX2 problems in MathGeoLib

--- math.cmake
+++ math.cmake
@@ -1,4 +1,10 @@
 macro(use_mgl MGL_PATH)
+    IF(WIN32)
+        add_compile_options(/arch:AVX2)
+    ELSE()
+        add_compile_options(-mavx2)
+    ENDIF()
+
     add_definitions(-DMATH_USE_OPENGL)
     add_definitions(-DMATH_RIGHTHANDED_CAMERA)
     add_definitions(-DMATH_AVX)
