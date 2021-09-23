dd86bf63150b0f50dcc4a53b79df6cba63b04851
hyzboy <hyzboy@gmail.com>
2021-09-24 01:05:10 +0800

    use glm instead of MathGeoLib

--- math.cmake
+++ math.cmake
@@ -1,14 +1,6 @@
-macro(use_mgl MGL_PATH)
-    IF(WIN32)
-        add_compile_options(/arch:AVX2)
-    ELSE()
-        add_compile_options(-mavx2)
-    ENDIF()
+macro(use_glm GLM_PATH)
 
-    add_definitions(-DMATH_USE_OPENGL)
-    add_definitions(-DMATH_RIGHTHANDED_CAMERA)
-    add_definitions(-DMATH_AVX)
+    include_directories(${GLM_PATH})
+    add_subdirectory(${GLM_PATH})
 
-    include_directories(${MGL_PATH}/src)
-    add_subdirectory(${MGL_PATH})
 endmacro()
