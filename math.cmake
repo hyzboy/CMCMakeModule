17e263bf440f0b8b6834b454a4952d2f2a38ce3c
hyzboy <hyzboy@gmail.com>
2024-08-05 01:23:54 +0800

    added GLM_FORCE_AVX2 macro

--- math.cmake
+++ math.cmake
@@ -5,6 +5,8 @@ add_definitions(-DGLM_FORCE_DEPTH_ZERO_TO_ONE)
 add_definitions(-DGLM_ENABLE_EXPERIMENTAL)
 
 if(WIN32)
+    add_definitions(-DGLM_FORCE_AVX2)
+    
     SET(HGL_MATH_LIB glm::glm)
 else()
     SET(HGL_MATH_LIB GLM)
