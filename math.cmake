795ee236e386262fe0fa6e2affdc94a5f9d4657f
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-10-16 00:22:40 +0800

    移除GLM_FORCE_INTRINSICS

--- math.cmake
+++ math.cmake
@@ -4,7 +4,6 @@ add_definitions(-DGLM_FORCE_RADIANS)
 add_definitions(-DGLM_FORCE_DEPTH_ZERO_TO_ONE)
 add_definitions(-DGLM_ENABLE_EXPERIMENTAL)
 add_definitions(-DGLM_FORCE_DEFAULT_ALIGNED_GENTYPES)
-add_definitions(-DGLM_FORCE_INTRINSICS)
 
 if(WIN32)
     add_definitions(-DGLM_FORCE_AVX2)
